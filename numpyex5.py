6d9daa4a471b7e26989d70cabff39fed9a308e6c
ariawanzero <ariawan.zero@gmail.com>
2016-08-15 16:04:33 +0700

    Learn Phyton Numpy

---
+++ numpyex5.py
@@ -0,0 +1,9 @@
+#UNIVERSAL FUNCTIONS
+import numpy as np
+B = np.arange(3)
+print "B = ", B
+print "np.exp(B) = ", np.exp(B)
+print "np.sqrt(B) = ", np.sqrt(B)
+
+C = np.array([2., -1., 4.])
+print "np.add(B, C) = ", np.add(B, C)
