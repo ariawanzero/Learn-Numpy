24a7ef624e56af546e3657ea9734f39de6bdd43c
ariawanzero <ariawan.zero@gmail.com>
2016-08-22 12:03:24 +0700

    Remove file

--- numpyex5.py
+++
@@ -1,9 +0,0 @@
-#UNIVERSAL FUNCTIONS
-import numpy as np
-B = np.arange(3)
-print "B = ", B
-print "np.exp(B) = ", np.exp(B)
-print "np.sqrt(B) = ", np.sqrt(B)
-
-C = np.array([2., -1., 4.])
-print "np.add(B, C) = ", np.add(B, C)
